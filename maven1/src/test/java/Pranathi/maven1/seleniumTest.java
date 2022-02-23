ea0b7754a1cd74dc47c2afe78be6a7c9197bfb71
Pranathi <pranathi.tummala0225@gmail.com>
2022-02-23 15:48:46 +0530

    commit

---
+++ maven1/src/test/java/Pranathi/maven1/seleniumTest.java
@@ -0,0 +1,16 @@
+package Pranathi.maven1;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+
+	@Test
+	public void A() {
+		System.out.println("I am in A1 method");
+	}
+	@Test
+	public void B() {
+		System.out.println("I am in B1 method");
+	}
+}
+
